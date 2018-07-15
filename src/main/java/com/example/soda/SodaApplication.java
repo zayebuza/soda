fe9159ae4e509f8d6fc58bfdca445f893c277b67
Shanks <24303369@qq.com>
2018-07-15 11:02:13 +0800

    init project

---
+++ src/main/java/com/example/soda/SodaApplication.java
@@ -0,0 +1,12 @@
+package com.example.soda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SodaApplication.class, args);
+	}
+}
